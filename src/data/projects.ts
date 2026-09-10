f825f6a4c110a018f3f77cb65d519262356467ed
landiluigi746 <landiluigi865@gmail.com>
2026-09-10 23:06:34 +0200

    [feat]: Add new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,13 +9,11 @@ export type ProjectProps = {
 
 export const projects: ProjectProps[] = [
   {
-    name: "Nova",
+    name: "ggpkg",
     description:
-      "Lightweight 2D game development framework written in C++20 using OpenGL 3.3. Designed to make building simple 2D games and applications easier and more straight-forward.",
-    imageURL:
-      "https://github.com/landiluigi746/Nova/blob/master/assets/NovaLogo.png?raw=true",
-    tags: ["C++", "OpenGL", "Game Dev"],
-    githubLink: "https://github.com/landiluigi746/Nova",
+      "ggpkg is a unified command-line interface for system package managers across different platforms written in C++23. It automatically detects your system's package manager and provides a consistent set of commands, making package management seamless whether you're on Windows, Ubuntu, Arch Linux, or other supported distributions.",
+    tags: ["CLI tool", "C++"],
+    githubLink: "https://github.com/landiluigi746/ggpkg",
   },
 
   {
@@ -23,7 +21,7 @@ export const projects: ProjectProps[] = [
     description:
       "An intuitive Windows 10/11 tool that simplifies PC setup and performance tuning with just a few clicks, streamlining system configuration for both casual and power users.",
     imageURL: "https://landiluigi746.github.io/Lufrak/images/Menu.png",
-    tags: ["C", "HTML", "CSS", "JavaScript"],
+    tags: ["Windows tool", "C", "HTML", "CSS", "JavaScript"],
     githubLink: "https://github.com/landiluigi746/Lufrak",
     websiteLink: "https://landiluigi746.github.io/Lufrak",
   },
@@ -34,18 +32,17 @@ export const projects: ProjectProps[] = [
       "A cellular automata simulator written in C++ with raylib and OpenMP. It provides users a simple interface to run various cellular automata on a 2D grid of cells.",
     imageURL:
       "https://github.com/landiluigi746/cellarion/blob/master/res/screenshots/BrianBrain.png?raw=true",
-    tags: ["C++", "Simulation"],
+    tags: ["Simulation", "C++"],
     githubLink: "https://github.com/landiluigi746/cellarion",
   },
 
   {
-    name: "projectify",
+    name: "The Essence",
     description:
-      "A simple and intuitive project management tool that helps you organize your work efficiently. Its backend is built with C++ and PostgreSQL as database, while the frontend, a simple and easy to use CLI app, is built with C++ and ftxui.",
-    imageURL:
-      "https://github.com/landiluigi746/projectify/blob/master/screenshots/frontend-cli2.png?raw=true",
-    tags: ["C++", "PostgreSQL", "Docker"],
-    githubLink: "https://github.com/landiluigi746/projectify",
+      "The Essence is a videogame developed for the 2026 Phaser Game Jam that led my team to the victory. The theme of the jam was (R)Evolution and the videogame consists in the concept of Conway's Game of Life, representing the Evolution, applied to a turn-based 1v1 game, using natural evolution and special cards that affect the game's board, (hence the Revolution)",
+    imageURL: "",
+    tags: ["Game Dev", "TypeScript", "Phaser"],
+    websiteLink: "https://the-essence-game.web.app/",
   },
 
   {
@@ -53,18 +50,18 @@ export const projects: ProjectProps[] = [
     description:
       "Caos Arcade is a videogame developed for the 2025 Phaser Game Jam (4th place out of 26 teams). The theme was Chaos and the videogame revolves around the idea of the gaming world being discombobulated by a deity of chaos, Nixaroth.",
     imageURL: "/portfolio/img/CaosArcade.png",
-    tags: ["TypeScript", "Phaser", "Game Dev"],
+    tags: ["Game Dev", "TypeScript", "Phaser"],
     websiteLink: "https://caosarcade.web.app/",
   },
 
   {
-    name: "shurlx",
+    name: "Nova",
     description:
-      "A lightweight, self-hostable URL shortener with a C++ htmx-based backend and SQLite database. Features Docker support for easy deployment, making it ideal for personal use or small-scale link management solutions.",
+      "Lightweight 2D game development framework written in C++20 using OpenGL 3.3. Designed to make building simple 2D games and applications easier and more straight-forward.",
     imageURL:
-      "https://github.com/landiluigi746/shurlx-old/blob/master/screenshots/example.png?raw=true",
-    tags: ["C++", "HTMX", "TailwindCSS", "Docker", "SQLite"],
-    githubLink: "https://github.com/landiluigi746/shurlx-old",
+      "https://github.com/landiluigi746/Nova/blob/master/assets/NovaLogo.png?raw=true",
+    tags: ["Game Dev", "C++", "OpenGL"],
+    githubLink: "https://github.com/landiluigi746/Nova",
   },
 ];
 
