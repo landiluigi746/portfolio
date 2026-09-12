be1523803dcf7c3ec28d46476adc038dbe1bca0e
landiluigi746 <landiluigi865@gmail.com>
2026-09-12 17:48:16 +0200

    [feat]: Add a new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,7 +12,7 @@ export const projects: ProjectProps[] = [
     name: "ggpkg",
     description:
       "ggpkg is a unified command-line interface for system package managers across different platforms written in C++23. It automatically detects your system's package manager and provides a consistent set of commands, making package management seamless whether you're on Windows, Ubuntu, Arch Linux, or other supported distributions.",
-    tags: ["CLI tool", "C++"],
+    tags: ["CLI tool", "C++", "CMake"],
     githubLink: "https://github.com/landiluigi746/ggpkg",
   },
 
@@ -32,8 +32,26 @@ export const projects: ProjectProps[] = [
       "A cellular automata simulator written in C++ with raylib and OpenMP. It provides users a simple interface to run various cellular automata on a 2D grid of cells.",
     imageURL:
       "https://github.com/landiluigi746/cellarion/blob/master/res/screenshots/BrianBrain.png?raw=true",
-    tags: ["Simulation", "C++"],
+    tags: ["Simulation", "C++", "CMake"],
     githubLink: "https://github.com/landiluigi746/cellarion",
+  },
+
+  {
+    name: "cstl",
+    description:
+      "A lightweight header/source C99 library designed to attempt a recreation of key C++ Standard Template Library (STL) features in pure C.",
+    tags: ["C", "CMake"],
+    githubLink: "https://github.com/landiluigi746/cstl",
+  },
+
+  {
+    name: "Nova",
+    description:
+      "Lightweight 2D game development framework written in C++20 using OpenGL 3.3. Designed to make building simple 2D games and applications easier and more straight-forward.",
+    imageURL:
+      "https://github.com/landiluigi746/Nova/blob/master/assets/NovaLogo.png?raw=true",
+    tags: ["Game Dev", "C++", "OpenGL", "CMake"],
+    githubLink: "https://github.com/landiluigi746/Nova",
   },
 
   {
@@ -52,16 +70,6 @@ export const projects: ProjectProps[] = [
     imageURL: "/portfolio/img/CaosArcade.png",
     tags: ["Game Dev", "TypeScript", "Phaser"],
     websiteLink: "https://caosarcade.web.app/",
-  },
-
-  {
-    name: "Nova",
-    description:
-      "Lightweight 2D game development framework written in C++20 using OpenGL 3.3. Designed to make building simple 2D games and applications easier and more straight-forward.",
-    imageURL:
-      "https://github.com/landiluigi746/Nova/blob/master/assets/NovaLogo.png?raw=true",
-    tags: ["Game Dev", "C++", "OpenGL"],
-    githubLink: "https://github.com/landiluigi746/Nova",
   },
 ];
 
